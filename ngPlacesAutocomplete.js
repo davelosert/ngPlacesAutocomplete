76840cb6f3c5e80d0d222c6cd0dab9ecb0d96bd7
Dave <david.losert@gmx.de>
2014-10-06 22:08:47 +0200

    Implemented textbase-search by doing a huge refactoring to put functionality into the directives controller.

--- ngPlacesAutocomplete.js
+++ ngPlacesAutocomplete.js
@@ -23,14 +23,14 @@ angular.module('ngPlacesAutocomplete', [])
 
 
 		return {
-			restrict : 'EA',
-			scope    : {
-				ngModel          : '=', // Query-Model
-				paOnPlaceReady   : '=',  // Callback for ready Details
-				paOptions        : '=?' // Options for autocomplete
+			restrict   : 'EA',
+			scope      : {
+				ngModel        : '=', // Query-Model
+				paOnPlaceReady : '=',  // Callback for ready Details
+				paOptions      : '=?' // Options for autocomplete
 			},
-			link     : function (scope, element, attributes) {
-				if (!angular.isFunction(scope.paOnPlaceReady)) {
+			controller : function ($scope) {
+				if (!angular.isFunction($scope.paOnPlaceReady)) {
 					throw new Error(moduleString + 'paOnPlaceReady needs to be a function!');
 				}
 
@@ -39,8 +39,49 @@ angular.module('ngPlacesAutocomplete', [])
 					throw new Error(moduleString + 'Google Places Service not found!');
 				}
 
-				// Instantaite the autocompleteion feature on the current element
+				var acService = new google.maps.places.AutocompleteService(),
+					// we need a dummy div since the placesService needs an HTML Element (usually a map) to be instantiated
+					dummyDiv = angular.element('<div>')[0],
+					placesService = new google.maps.places.PlacesService(dummyDiv);
+
+				/**
+				 * Manual search bound to controller so it can get executed by
+				 */
+				this.manualSearch = function () {
+					var queryObject = angular.extend({
+						input : $scope.ngModel
+					}, $scope.paOptions);
+
+					acService.getPlacePredictions(queryObject, function (predictionResults) {
+						if (!predictionResults || predictionResults.length == 0) {
+							return $scope.onDetailsReady(null);
+						}
+						placesService.getDetails({placeId : predictionResults[0].place_id}, function (place, status) {
+							if (status == google.maps.places.PlacesServiceStatus.OK
+								|| status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+								$scope.paOnPlaceReady(null, place);
+							}
+							else {
+								$scope.paOnPlaceReady(new Error(moduleString + 'Places returned status: "' + status + '"'));
+							}
+							$scope.$apply();
+						});
+					});
+				};
+			},
+			link       : function (scope, element, attributes, controller) {
+				// Instantiate the autocompleteion feature on the current element
 				var autocomplete = new google.maps.places.Autocomplete(element[0], {});
+
+				/**
+				 * On place_changed event
+				 */
+				google.maps.event.addListener(autocomplete, 'place_changed', function () {
+					scope.$apply(function () {
+						var result = autocomplete.getPlace();
+						result.address_components ? scope.paOnPlaceReady(null, result) : controller.manualSearch();
+					});
+				});
 
 				/**
 				 * Function to set Options to the autocomplete-instance.
@@ -65,15 +106,6 @@ angular.module('ngPlacesAutocomplete', [])
 						});
 					}
 				}
-
-				/**
-				 * On place_changed event
- 				 */
-				google.maps.event.addListener(autocomplete, 'place_changed', function () {
-					scope.$apply(function () {
-						scope.paOnPlaceReady(autocomplete.getPlace());
-					});
-				});
 
 				scope.$watch('paOptions', function () {
 					setOptions();
